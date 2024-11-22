e02219a0951d95015a2c6905fc9b8f6194856a26
niloychandrasarker <niloysarker.cs@gmail.com>
2024-11-22 23:08:59 +0600

    Add JavaScript

---
+++ 001-counter-app/script.js
@@ -0,0 +1,22 @@
+
+const incrimentButton = document.getElementById('increment');
+const decrimentButton = document.getElementById('decrement');
+const countterElement = document.getElementById('counter');
+
+let count=0;
+function increaseCount(){
+    //alert('Incriment Button Clicked')
+    count = count+1;
+    countterElement.innerText = count;
+}
+
+
+function decrimentCount(){
+    //alert('Decriment Button Clicked')
+    count = count-1;
+    countterElement.innerText = count;
+}
+
+incrimentButton.addEventListener('click', increaseCount);
+
+decrimentButton.addEventListener('click', decrimentCount);
