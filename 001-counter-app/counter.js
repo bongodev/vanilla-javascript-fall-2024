7aae9889579f75816ccdc9fccad0480818015477
dipuMojumder <mojumderdipu66@gmail.com>
2024-11-24 22:22:15 +0600

    added condition

--- 001-counter-app/counter.js
+++ 001-counter-app/counter.js
@@ -2,14 +2,26 @@ let count=0;
 const counterElement=document.getElementById('counter');
 const incrementButton=document.getElementById('increment');
 function increaseCount(){
+    if(count>10)
+    {
+        alert('count overflow');
+    }
+    else{
     count=count+1;
     counterElement.innerText=count;
+    }
 }
 incrementButton.addEventListener('click', increaseCount);
 function decrementCount()
 {
+    if(count==0)
+    {
+        alert('count will be negative');
+    }
+    else{
     count=count-1;
     counterElement.innerText=count;
+    }
 }
 const decrementButton=document.getElementById('decrement');
 decrementButton.addEventListener('click',decrementCount );
