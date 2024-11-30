93c837691bb726dcd4b8fc874e69bb812054ca3a
niloychandrasarker <niloysarker.cs@gmail.com>
2024-11-30 14:20:24 +0600

    Add Valid Input Funtionality

--- 002-number-comparator/Niloy/number-comparator.js
+++ 002-number-comparator/Niloy/number-comparator.js
@@ -3,10 +3,33 @@ const number2Input=document.getElementById('number2');
 const compareButton = document.getElementById('compare');
 const result = document.getElementById('result');
 
+const ERROR_CLASS = 'border-red-500';
 
 
+
+function resetErrorStyles() {
+    number1Input.classList.remove(ERROR_CLASS);
+    number2Input.classList.remove(ERROR_CLASS);
+  }
+  
+  function isValidInput() {
+    resetErrorStyles();
+  
+    if (!number1Input.value) {
+      number1Input.classList.add(ERROR_CLASS);
+      return false;
+    }
+    if (!number2Input.value) {
+      numberI2nput.classList.add(ERROR_CLASS);
+      return false;
+    }
+  
+    return true;
+  }
 
 function handleCompareButton(){
+
+    if (!isValidInput()) return;
 
     number1=parseInt(number1Input.value);
     number2=parseInt(number2Input.value);
