76c89e7d4e167a556b883eca51dfdc69e11ad7f3
Tamjidul-Islam <tamjidtj6@gmail.com>
2024-12-08 18:48:58 +0600

    Added colours to the table

--- Tamjid/script.js
+++ Tamjid/script.js
@@ -1,17 +1,15 @@
-const numberInput = document.getElementById('input-number');
-const generateBtn = document.getElementById('generate');
-const tableBody = document.getElementById('table-body');
-const errorMessage = document.getElementById('error-message');
-const resetButton = document.getElementById('reset-btn');
+const numberInput = document.getElementById("input-number");
+const generateBtn = document.getElementById("generate");
+const tableBody = document.getElementById("table-body");
+const errorMessage = document.getElementById("error-message");
+const resetButton = document.getElementById("reset-btn");
 
-
-generateBtn.addEventListener('click', function () {
-
-  if(numberInput.value==""){
-    errorMessage.innerText="Please enter valid input";
+generateBtn.addEventListener("click", function () {
+  if (numberInput.value == "") {
+    errorMessage.innerText = "Please enter valid input";
     errorMessage.classList.remove("hidden");
     numberInput.classList.add("border-red-500", "bg-red-100");
-    return; 
+    return;
   }
   const num = parseInt(numberInput.value);
   numberInput.classList.remove("border-red-500", "bg-red-100");
@@ -21,12 +19,18 @@ generateBtn.addEventListener('click', function () {
 });
 
 function cleanTable() {
-  tableBody.innerHTML = '';
+  tableBody.innerHTML = "";
 }
 
 function generateTable(num) {
   for (let rowNo = 1; rowNo <= 10; rowNo++) {
     const tableRow = generateRow(num, rowNo);
+    if (rowNo % 2 === 0) {
+      tableRow.style.backgroundColor = "#a8a8a8";
+    } else {
+      tableRow.style.backgroundColor = "#f0f0f0";
+    }
+
     tableBody.appendChild(tableRow);
   }
 }
@@ -34,17 +38,17 @@ function generateTable(num) {
 function generateRow(num, rowNo) {
   const cells = [];
   for (let i = 1; i <= 5; i++) {
-    const cell = document.createElement('td');
+    const cell = document.createElement("td");
     cells.push(cell);
   }
 
   cells[0].innerText = num;
-  cells[1].innerText = ' x ';
+  cells[1].innerText = " x ";
   cells[2].innerText = rowNo;
-  cells[3].innerText = ' = ';
+  cells[3].innerText = " = ";
   cells[4].innerText = num * rowNo;
 
-  const tableRow = document.createElement('tr');
+  const tableRow = document.createElement("tr");
 
   for (let i = 0; i < cells.length; i++) {
     tableRow.appendChild(cells[i]);
@@ -52,7 +56,7 @@ function generateRow(num, rowNo) {
 
   return tableRow;
 }
-resetButton.addEventListener('click', function () {
+resetButton.addEventListener("click", function () {
   numberInput.value = "";
   cleanTable();
   errorMessage.innerText = "";
