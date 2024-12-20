668ff8da8a20b7380dfe2d180482d0d9c6e693e9
Zubayer03Mahmud <zubayer.mahmud03@gmail.com>
2024-12-20 14:25:29 +0600

    Added counter app resolve

--- 001-counter-app/zubayer/script.js
+++ 001-counter-app/zubayer/script.js
@@ -1,27 +1,21 @@
-console.log('JavaScript added');
-let titleElement = document.getElementById('title');
-titleElement.innerText = 'Counter App';
-
 let count = 0;
-const counterElement = document.getElementById('counter');
-const incrementButton = document.getElementById('increment');
-const decrementButton = document.getElementById('decrement');
+const counterElement = document.getElementById("counter");
+const incrementButton = document.getElementById("increment");
+const decrementButton = document.getElementById("decrement");
 
 function increaseCount() {
   if (count === 10) {
-    alert('Count overflow!');
+    alert("Count overflow!");
+    return;
   }
-  else{
   count++;
-  counterElement.innerText = count;    
-  }
-
+  counterElement.innerText = count;
 }
 
-incrementButton.addEventListener('click', increaseCount);
-decrementButton.addEventListener('click', function () {
+incrementButton.addEventListener("click", increaseCount);
+decrementButton.addEventListener("click", function () {
   if (count === 0) {
-    alert('Count will be negative');
+    alert("Count will be negative");
   } else {
     count--;
     counterElement.innerText = count;
