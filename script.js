d28d94de0aba5f88e9d93803de0a4b02a7265b25
Fariha Promi <farihapromi2611@gmail.com>
2024-12-29 22:46:34 +0600

    resolve issues with my name as a contributor

--- script.js
+++ script.js
@@ -1,83 +1,92 @@
 const projects = [
   {
-    name: "Counter App",
-    dirPath: "001-counter-app",
+    name: 'Counter App',
+    dirPath: '001-counter-app',
   },
   {
-    name: "Number Comparator",
-    dirPath: "002-number-comparator",
+    name: 'Number Comparator',
+    dirPath: '002-number-comparator',
   },
   {
-    name: "Grade Calculator",
-    dirPath: "003-grade-calculator",
+    name: 'Grade Calculator',
+    dirPath: '003-grade-calculator',
   },
   {
-    name: "Number Table",
-    dirPath: "004-number-table",
+    name: 'Number Table',
+    dirPath: '004-number-table',
   },
   {
-    name: "Number Table of 1-10",
-    dirPath: "005-number-table-1-10",
+    name: 'Number Table of 1-10',
+    dirPath: '005-number-table-1-10',
   },
   {
-    name: "Employee Management",
-    dirPath: "006-employee-table",
+    name: 'Employee Management',
+    dirPath: '006-employee-table',
   },
   {
-    name: "Simple E-commerce",
-    dirPath: "007-e-commerce",
+    name: 'Simple E-commerce',
+    dirPath: '007-e-commerce',
+  },
+  {
+    name: 'Render Products (Promise, async-await)',
+    dirPath: '008-render-products',
   },
 ];
 
 const contributors = [
   {
-    name: "BongoDev",
-    dirPath: "bongodev",
-    email: "support@bongodev.com",
+    name: 'BongoDev',
+    dirPath: 'bongodev',
+    email: 'support@bongodev.com',
   },
   {
-    name: "Nazma",
-    dirPath: "nazma98",
-    email: "nazma.sarker1398@gmail.com",
+    name: 'Nazma',
+    dirPath: 'nazma98',
+    email: 'nazma.sarker1398@gmail.com',
   },
   {
-    name: "Talha",
-    dirPath: "Talha",
-    email: "ameertalha22@gmail.com",
+    name: 'Talha',
+    dirPath: 'Talha',
+    email: 'ameertalha22@gmail.com',
   },
   {
-    name: "Md. Al Amin",
-    dirPath: "Alamin",
-    email: "aaalaamin7@gmail.com",
+    name: 'Md. Al Amin',
+    dirPath: 'Alamin',
+    email: 'aaalaamin7@gmail.com',
   },
   {
-    name: "Sumiya Yasmin",
-    dirPath: "sumiya-yasmin",
-    email: "sumiyayasmin665@gmail.com",
+    name: 'Sumiya Yasmin',
+    dirPath: 'sumiya-yasmin',
+    email: 'sumiyayasmin665@gmail.com',
   },
   {
-    name: "Than Win",
-    dirPath: "thanwin",
-    email: "thanwinhline84@gmail.com",
+    name: 'Than Win',
+    dirPath: 'thanwin',
+    email: 'thanwinhline84@gmail.com',
   },
   {
-    name: "Tamjid",
-    dirPath: "Tamjid",
-    email: "tamjidtj6@gmail.com",
+    name: 'Tamjid',
+    dirPath: 'Tamjid',
+    email: 'tamjidtj6@gmail.com',
   },
   {
-    name: "Dipu",
-    dirPath: "Dipu",
-    email: "mojumderdipu66@gmail.com",
+    name: 'Dipu',
+    dirPath: 'Dipu',
+    email: 'mojumderdipu66@gmail.com',
   },
   {
-    name: "Promi",
-    dirPath: "promi",
-    email: "farihapromi2611@gmail.com",
+    name: 'Sakib',
+    dirPath: 'Sakib',
+    email: 'nazmussakib06927@gmail.com',
+  },
+  {
+    name: 'Promi',
+    dirPath: 'promi',
+    email: 'farihapromi2611@gmail.com',
   },
 ];
 
-const projectsContainer = document.getElementById("projects-container");
+const projectsContainer = document.getElementById('projects-container');
 
 renderProjectsAndContributors();
 
@@ -89,7 +98,7 @@ function renderProjectsAndContributors() {
 }
 
 function getProjectSection(project) {
-  const projectSection = document.createElement("div");
+  const projectSection = document.createElement('div');
 
   const projectTitle = getProjectTitle(project.name);
   projectSection.appendChild(projectTitle);
@@ -101,21 +110,21 @@ function getProjectSection(project) {
 }
 
 function getProjectTitle(projectName) {
-  const projectTitle = document.createElement("h1");
-  projectTitle.className = "text-2xl font-bold text-gray-800 mb-4";
+  const projectTitle = document.createElement('h1');
+  projectTitle.className = 'text-2xl font-bold text-gray-800 mb-4';
   projectTitle.innerText = projectName;
   return projectTitle;
 }
 
 function getContributors(projectPath) {
-  const contributorsList = document.createElement("ul");
-  contributorsList.className = "list-disc list-inside";
+  const contributorsList = document.createElement('ul');
+  contributorsList.className = 'list-disc list-inside';
 
   contributors.forEach((contributor) => {
-    const listItem = document.createElement("li");
+    const listItem = document.createElement('li');
 
-    const projectLink = document.createElement("a");
-    projectLink.className = "text-blue-600 visited:text-purple-600";
+    const projectLink = document.createElement('a');
+    projectLink.className = 'text-blue-600 visited:text-purple-600';
     projectLink.innerText = contributor.name;
     projectLink.href = `./${projectPath}/${contributor.dirPath}/index.html`;
 
