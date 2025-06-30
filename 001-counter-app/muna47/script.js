dacf4bbff04ff7543b41d39d589bc1957d9855f6
Mymuna Islam <mymunai552@gmail.com>
2025-06-30 23:21:05 +0600

    updated script for more efficiency

--- 001-counter-app/muna47/script.js
+++ 001-counter-app/muna47/script.js
@@ -1,23 +1,20 @@
 var counterVar = document.getElementById("counter");
-
 const incrementButton = document.getElementById("increment");
-function increment() {
-  let current = parseInt(counterVar.innerText, 10);
-  if (current < 10) counterVar.innerText = current + 1;
-  else alert("count cannot be greater than 10");
-}
-incrementButton.addEventListener("click", increment);
-
 const decrementButton = document.getElementById("decrement");
-function decrement() {
-  let current = parseInt(counterVar.innerText, 10);
-  if (current > 0) counterVar.innerText = current - 1;
-  else alert("count cannot be less than 0");
-}
-decrementButton.addEventListener("click", decrement);
-
 const resetButton = document.getElementById("reset");
-function reset() {
+
+let current = parseInt(counterVar.innerText, 10);
+
+incrementButton.addEventListener("click", function () {
+  if (current < 10) counterVar.innerText = ++current;
+  else alert("count cannot be greater than 10");
+});
+
+decrementButton.addEventListener("click", function () {
+  if (current > 0) counterVar.innerText = --current;
+  else alert("count cannot be less than 0");
+});
+
+resetButton.addEventListener("click", function () {
   counterVar.innerText = "0";
-}
-resetButton.addEventListener("click", reset);
+});
