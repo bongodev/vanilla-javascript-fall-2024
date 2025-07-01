92864b1a345773f2aae5df041729549b904e7f90
Mymuna Islam <mymunai552@gmail.com>
2025-07-01 23:59:52 +0600

    completed number comparator

--- 002-number-comparator/muna47/script.js
+++ 002-number-comparator/muna47/script.js
@@ -5,11 +5,34 @@ const result = document.getElementById("result");
 const num1 = document.getElementById("number1");
 const num2 = document.getElementById("number2");
 let res = "";
+const errorClass = "border-red-500";
+
+function reset() {
+  num1.classList.remove(errorClass);
+  num2.classList.remove(errorClass);
+  result.innerHTML = "";
+}
+
+function validInputs() {
+  reset();
+  if (num1.value === "") {
+    num1.classList.add(errorClass);
+    return false;
+  }
+  if (num2.value === "") {
+    num2.classList.add(errorClass);
+    return false;
+  }
+  return true;
+}
 
 compBtn.addEventListener("click", function () {
-  const number1 = parseInt(num1.value, 10);
-  const number2 = parseInt(num2.value, 10);
-  let res = "";
+  if (!validInputs()) {
+    result.innerHTML = "Enter both numbers!!";
+    return;
+  }
+  const number1 = parseInt(num1.value);
+  const number2 = parseInt(num2.value);
 
   if (number1 < number2) res = `${number1} is less than ${number2}`;
   else if (number1 > number2) res = `${number1} is greater than ${number2}`;
@@ -18,6 +41,8 @@ compBtn.addEventListener("click", function () {
 });
 
 resetBtn.addEventListener("click", function () {
-  num1.value = "0";
-  num2.value = "0";
+  num1.value = "";
+  num2.value = "";
+  reset();
+  result.innerHTML = "";
 });
