c32f935dc9739109bc6f930502a4f2975c430bdf
Mymuna Islam <mymunai552@gmail.com>
2025-07-02 21:56:02 +0600

    fixed minor issues

--- 003-grade-calculator/muna47/script.js
+++ 003-grade-calculator/muna47/script.js
@@ -6,32 +6,39 @@ let res = "";
 
 const errorClass = "border-red-500";
 const errorText = "text-red-500";
+
+calc_btn.addEventListener("click", function () {
+  number = parseFloat(num.value);
+  if (!validInput(number)) {
+    return;
+  }
+  calculate_grade(number);
+
+  result.innerText = res;
+});
+
 function reset() {
   num.classList.remove(errorClass);
   result.classList.remove(errorText);
   result.innerText = "";
 }
 
-function validInput() {
+function validInput(number) {
   reset();
-  if (num.value == "") {
+  if (!num.value) {
     result.innerText = "Enter valid input!!";
     result.classList.add(errorText);
     num.classList.add(errorClass);
     return false;
-  } else if (parseInt(num.value) < 0 || parseInt(num.value) > 100) {
+  } else if (number < 0 || number > 100) {
     result.innerText = "Enter number between 0 and 100!!";
     result.classList.add(errorText);
     return false;
   }
   return true;
 }
-calc_btn.addEventListener("click", function () {
-  if (!validInput()) {
-    return;
-  }
 
-  number = parseInt(num.value);
+function calculate_grade(number) {
   if (number < 33) res = "F";
   else if (number <= 40) res = "D";
   else if (number <= 50) res = "C";
@@ -39,9 +46,9 @@ calc_btn.addEventListener("click", function () {
   else if (number <= 70) res = "A-";
   else if (number <= 80) res = "A";
   else res = "A+";
-  result.innerText = res;
-});
 
+  return res;
+}
 reset_btn.addEventListener("click", function () {
   result.innerText = "";
   num.value = "";
