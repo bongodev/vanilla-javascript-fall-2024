fcf6d2c9790d9c26454e1dd1682af3ac5029a7d7
TasinLunatic <ahmedtasin207@gmail.com>
2025-07-08 00:59:04 +0600

    Add number comparator

--- 001-counter-app/TasinLunatic/script.js
+++ 001-counter-app/TasinLunatic/script.js
@@ -1,5 +1,28 @@
+let counter=0;
+
+const counterDisplay=document.getElementById('counter');
 const decrementButton =document.getElementById('decrement');
 const incrementButton =document.getElementById('increment');
+
+
+function isLargerThanTen(number){
+    /*if(number>10){
+        return true;
+    }else{
+        return false;
+    }*/
+   return number > 10 ? true:false;
+  
+}
+
+function increment(){
+    if(counter===10){
+        alert("Counter overflow");
+        return;
+    }
+    counter++;
+    counterDisplay.innerText=counter;
+}
 
 function increment(){
     alert("increment button has been clicked");
