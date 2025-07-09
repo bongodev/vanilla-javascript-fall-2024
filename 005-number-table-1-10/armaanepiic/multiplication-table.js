7d3e755b9575abf3bd70d0e5714d52e936b92698
arman <armaanepiicfb17@gmail.com>
2025-07-09 13:23:52 +0600

    create a table function, and table body function

--- 005-number-table-1-10/armaanepiic/multiplication-table.js
+++ 005-number-table-1-10/armaanepiic/multiplication-table.js
@@ -1,63 +1,30 @@
-const numberInput = document.getElementById('input-number');
 const generateBtn = document.getElementById('generate');
-const tableBody = document.getElementById('table-body');
+const tableContainerTopRow = document.getElementById('top-row');
+const tableContainerBottomRow = document.getElementById('bottom-row');
 
-function generateRow({number, rowNo})
+function getTableRow({number, tRow})
 {
-    // creating row for the table
-    const tableRow = document.createElement('tr');
-
-    // cells array
-    const cells = [];
-    const rowData = [number, "X" , rowNo, "=", number*rowNo];
-    for(let i = 0; i < 5 ; i++){
-        const cell = document.createElement('td');
-        cell.classList.add('text-center');
-        cell.classList.add('px-4');
-        cell.classList.add('py-2');
-        cell.innerText = rowData[i];
-        cells.push(cell);
-    }
-   
-
-    // destructuring the cells array to the tableRow
-    tableRow.append(...cells);
-    return tableRow;
-}
-function generateTable(number)
-{
-    for(let rowNo = 1; rowNo <= 10 ; rowNo++)
-    {
-        const tableRow = generateRow({number, rowNo});
-        tableBody.appendChild(tableRow);
-
-        // applying a bg for even rows
-        if(rowNo % 2 == 0){
-            tableRow.classList.add('bg-gray-600');
-        }
-    }
-    
+	
 }
 
-function handleClick()
+function getTable({number})
 {
-    // make the table body empty before creating number table
-    tableBody.innerText = '';
-
-    const number = parseInt(numberInput.value);
-
-    // checking the valid input here
-    if(!number){
-        alert("enter valid number");
-        numberInput.value = 1;
-        return;
-    }
-
-    // calling the function to generate the table passing the number as argument
-    generateTable(number);
+	const numberTable = document.createElement('table');
+	numberTable.className = 'table-auto w-full border border-slate-700';
+	const tableBody = document.createElement('tbody');
+	for(let row = 1 ; row <= 10 ; row++)
+	{
+		const tableRow = getTableRow({number, row});
+		tableBody.appendChild(tableRow);
+	}
+	numberTable.appendChild(tableBody); 
+	return numberTable;
 }
 
-// add eventlistener to the input field
-numberInput.addEventListener('keydown', (e) => e.key === 'Enter' && handleClick());
-
-generateBtn.addEventListener('click', handleClick);
+generateBtn.addEventListener('click', () => {
+	for(let num = 1 ; num <= 10 ; num++) {
+		const numberTable = getTable(num);
+		if(num <= 5) tableContainerTopRow.appendChild(numberTable);
+		else tableContainerBottomRow.appendChild(numberTable);
+	}
+})
