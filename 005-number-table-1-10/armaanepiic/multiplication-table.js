e4770c22ce1f6a916b14cc43ea0e5e895a78b394
arman <armaanepiicfb17@gmail.com>
2025-07-10 00:29:18 +0600

    use Object.assign to copy cell

--- 005-number-table-1-10/armaanepiic/multiplication-table.js
+++ 005-number-table-1-10/armaanepiic/multiplication-table.js
@@ -1,43 +1,53 @@
-const generateBtn = document.getElementById('generate');
-const tableContainerTopRow = document.getElementById('top-row');
-const tableContainerBottomRow = document.getElementById('bottom-row');
+const generateBtn = document.getElementById("generate");
+const tableContainerTopRow = document.getElementById("top-row");
+const tableContainerBottomRow = document.getElementById("bottom-row");
 
-function getTableRow({number, rowNo})
-{
-	const tableRow = document.createElement('tr');
-	const cells = [];
-    const rowData = [number, "X" , rowNo, "=", number*rowNo];
+function getTableRow({ number, rowNo }) {
+  const tableRow = document.createElement("tr");
+  const rowData = [number, "*", rowNo, "=", number * rowNo];
 
-	rowData.forEach((data) => {
-		const cell = document.createElement('td');
-		cell.innerText = data;
-		cells.push(cell);
-	});
-	tableRow.append(...cells);
-    return tableRow;
+  //   const cells = rowData.map((data) => {
+  // 	const cell = document.createElement('td');
+  // 	cell.innerText = data;
+  // 	return cell;
+  //   });
+
+  //   rowData.forEach((data) => {
+  //     const cell = document.createElement("td");
+  //     cell.innerText = data;
+  //     cells.push(cell);
+  //   });
+  //   const tableData = document.createElement("td");
+
+  // Object.assign(target, source)
+  const cells = rowData.map((data) =>
+    Object.assign(document.createElement("td"), { innerText: data })
+  );
+  tableRow.append(...cells);
+  return tableRow;
 }
 
-function getTable(number)
-{
-	const numberTable = document.createElement('table');
-	numberTable.className = 'table-auto w-full border border-slate-700';
-	const tableBody = document.createElement('tbody');
-	for(let rowNo = 1 ; rowNo <= 10 ; rowNo++)
-	{
-		const tableRow = getTableRow( {number, rowNo} );
-		if(rowNo % 2 == 0){
-			tableRow.className = 'bg-slate-200';
-		}
-		tableBody.appendChild(tableRow);
-	}
-	numberTable.appendChild(tableBody); 
-	return numberTable;
+function getTable(number) {
+  const numberTable = document.createElement("table");
+  numberTable.className = "table-auto w-full border border-slate-700";
+  const tableBody = document.createElement("tbody");
+  for (let rowNo = 1; rowNo <= 10; rowNo++) {
+    const tableRow = getTableRow({ number, rowNo });
+    if (rowNo % 2 == 0) {
+      tableRow.className = "bg-slate-200";
+    }
+    tableBody.appendChild(tableRow);
+  }
+  numberTable.appendChild(tableBody);
+  return numberTable;
 }
 
-generateBtn.addEventListener('click', () => {
-	for(let num = 1 ; num <= 10 ; num++) {
-		const numberTable = getTable(num);
-		if(num <= 5) tableContainerTopRow.appendChild(numberTable);
-		else tableContainerBottomRow.appendChild(numberTable);
-	}
-})
+generateBtn.addEventListener("click", () => {
+  tableContainerTopRow.innerHTML = "";
+  tableContainerBottomRow.innerHTML = "";
+  for (let num = 1; num <= 10; num++) {
+    const numberTable = getTable(num);
+    if (num <= 5) tableContainerTopRow.appendChild(numberTable);
+    else tableContainerBottomRow.appendChild(numberTable);
+  }
+});
