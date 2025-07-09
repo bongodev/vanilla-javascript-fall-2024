c064b2bc95d31083d6657c411f1b39aaf4178576
arman <armaanepiicfb17@gmail.com>
2025-07-10 02:51:02 +0600

    formated

--- 006-employee-table/armaanepiic/script.js
+++ 006-employee-table/armaanepiic/script.js
@@ -11,11 +11,12 @@ const employeeData = [
     position: "Product Manager",
     department: "Product",
   },
-  { 
-    id: 3, 
-    name: "Sam Johnson", 
-    position: "Designer", 
-    department: "Design" },
+  {
+    id: 3,
+    name: "Sam Johnson",
+    position: "Designer",
+    department: "Design",
+  },
   {
     id: 4,
     name: "Chris Lee",
@@ -24,25 +25,25 @@ const employeeData = [
   },
 ];
 
-const employeeTableBody = document.getElementById('employeeTableBody');
+const employeeTableBody = document.getElementById("employeeTableBody");
 const getEmployeeRow = (employee) => {
-    const colNames = Object.keys(employee);
-    const columns = colNames.map((columnName) => {
-        const column = document.createElement('td');
-        column.className = 'border border-2 py-8 px-4';
-        column.innerText = employee[columnName];
-        return column;
-    });
-    const employeeRow = document.createElement('tr');
-    employeeRow.className = 'hover:bg-gray-200 text-center';
-    employeeRow.append(...columns);
-    return employeeRow;
-}
+  const colNames = Object.keys(employee);
+  const columns = colNames.map((columnName) => {
+    const column = document.createElement("td");
+    column.className = "border border-2 py-8 px-4";
+    column.innerText = employee[columnName];
+    return column;
+  });
+  const employeeRow = document.createElement("tr");
+  employeeRow.className = "hover:bg-gray-200 text-center";
+  employeeRow.append(...columns);
+  return employeeRow;
+};
 const renderEmployeeData = (employees) => {
-    employees.forEach(employee => {
-        const employeeRow = getEmployeeRow(employee);
-        employeeTableBody.appendChild(employeeRow);
-    });
-}
+  employees.forEach((employee) => {
+    const employeeRow = getEmployeeRow(employee);
+    employeeTableBody.appendChild(employeeRow);
+  });
+};
 
 renderEmployeeData(employeeData);
