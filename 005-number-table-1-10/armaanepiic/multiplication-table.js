888905884dcba82dc0a921490dcbc723c1d3a4ad
arman <armaanepiicfb17@gmail.com>
2025-07-09 13:41:29 +0600

    add function to create cells

--- 005-number-table-1-10/armaanepiic/multiplication-table.js
+++ 005-number-table-1-10/armaanepiic/multiplication-table.js
@@ -2,19 +2,34 @@ const generateBtn = document.getElementById('generate');
 const tableContainerTopRow = document.getElementById('top-row');
 const tableContainerBottomRow = document.getElementById('bottom-row');
 
-function getTableRow({number, tRow})
+function getTableRow({number, rowNo})
 {
-	
+
+	const tableRow = document.createElement('tr');
+	const cells = [];
+    const rowData = [number, "X" , rowNo, "=", number*rowNo];
+    for(let i = 0; i < 5 ; i++){
+        const cell = document.createElement('td');
+        cell.classList.add('text-center');
+        cell.classList.add('px-4');
+        cell.classList.add('py-2');
+        cell.innerText = rowData[i];
+        cells.push(cell);
+    }
+	tableRow.append(...cells);
+	console.log(tableRow);
+    return tableRow;
 }
 
-function getTable({number})
+function getTable(number)
 {
+	
 	const numberTable = document.createElement('table');
 	numberTable.className = 'table-auto w-full border border-slate-700';
 	const tableBody = document.createElement('tbody');
-	for(let row = 1 ; row <= 10 ; row++)
+	for(let rowNo = 1 ; rowNo <= 10 ; rowNo++)
 	{
-		const tableRow = getTableRow({number, row});
+		const tableRow = getTableRow({number, rowNo});
 		tableBody.appendChild(tableRow);
 	}
 	numberTable.appendChild(tableBody); 
