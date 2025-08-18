8147d0b87048d96a4d5cc8e9cc1d39a0bcccbe7a
TasinLunatic <ahmedtasin207@gmail.com>
2025-08-19 02:06:59 +0600

    cells added and destructured

--- 004-number-table/Tasin/multiplication-table.js
+++ 004-number-table/Tasin/multiplication-table.js
@@ -1,5 +1,6 @@
 const numberInput = document.getElementById('number-input');
 const generateBtn = document.getElementById('generate');
+const tableBody=document.getElementById('table-body');
 
 
 generateBtn.addEventListener('click',function(){
@@ -11,5 +12,30 @@ generateBtn.addEventListener('click',function(){
         alert('Enter a valid number.');
         return;
     }
+    //generate the multiplication table
+    generateTable(number);
 
 })
+
+function generateTable(number){
+    const row =generateRow({number:number,rowNo:3});
+    tableBody.appendChild(row);
+}
+
+
+function generateRow({number,rowNo}){
+    const row=document.createElement('tr');
+
+    const cells=[];
+    for(let i=1;i<=5;i++){
+        const cell=document.createElement('td');
+        cells.push(cell);
+    }
+    cells[0].innerText=number;
+    cells[1].innerText='*';
+    cells[2].innerText=rowNo;
+    cells[3].innerText='=';
+    cells[4].innerText=number*rowNo;
+
+    row.append(...cells);
+}
