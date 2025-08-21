66eed1eebdd0e4e87ebea71efe1a7ca0db3ca2f8
TasinLunatic <ahmedtasin207@gmail.com>
2025-08-22 01:29:54 +0600

    index.html and loop added in js

---
+++ 005-number-table-1-10/Tasin/multiplication-table.js
@@ -0,0 +1,11 @@
+const tablecontainerToprow=document.getElementById('toprow');
+const tablecontainerBottomrow =document.getElementById('bottomrow');
+const generateBtn = document.getElementById('generate');
+
+generateBtn.addEventListener('click',()=>{
+ for(let number=1;number<=10;number++){
+    const numberTable= getTable(number);
+
+    
+ }
+})
