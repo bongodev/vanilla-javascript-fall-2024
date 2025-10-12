222294b5532032c174e7db577b07b1eac8b70b30
parisa <rezaparisa5@gmail.com>
2025-10-12 20:06:43 +0600

    Parisa-Reza: Added Counter App

---
+++ 001-counter-app/Parisa-Reza/script.js
@@ -0,0 +1,24 @@
+const buttonIncrement = document.getElementById('increment');
+const buttonDecrement = document.getElementById('decrement');
+const countNumber =document.getElementById('counter');
+
+alert('connected !!!!!!');
+
+let count=0;
+
+function increaseNumber(){
+
+    count++;
+    countNumber.innerText= count;
+
+};
+
+function decreaseNumber(){
+
+    count--;
+    countNumber.innerText= count;
+
+};
+
+buttonIncrement.addEventListener('click',increaseNumber);
+buttonDecrement.addEventListener('click',decreaseNumber);
