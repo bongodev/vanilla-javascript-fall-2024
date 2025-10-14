da6ea3d7c41006c6f148fefcdedc7214ed2f69ef
irfan-mahbub <talha.mahbub.irfan@gmail.com>
2025-10-14 22:30:40 +0600

    irfan-mahbub: add counter app

---
+++ 001-counter-app/irfan-mahbub/script.js
@@ -0,0 +1,18 @@
+let count = 0;
+
+const counterElement = document.getElementById('counter')
+
+const incrementButton = document.getElementById('increment')
+const decrementButton = document.getElementById('decrement')
+
+function increaseCount() {
+    count++;
+    counterElement.innerText = count;
+}
+function decreaseCount() {
+    count--;
+    counterElement.innerText = count;
+}
+
+incrementButton.addEventListener("click", increaseCount);
+decrementButton.addEventListener("click", decreaseCount);
