25ad016a434c22dbc036263b37c2b0a541f73bfe
Ali Imam <ashovon832@gmail.com>
2025-10-24 01:53:04 +0600

    Fix the formatting issue in contributor file

--- script.js
+++ script.js
@@ -1,163 +1,163 @@
 const projects = [
   {
-    name: "Counter App",
-    dirPath: "001-counter-app",
+    name: 'Counter App',
+    dirPath: '001-counter-app',
   },
   {
-    name: "Number Comparator",
-    dirPath: "002-number-comparator",
+    name: 'Number Comparator',
+    dirPath: '002-number-comparator',
   },
   {
-    name: "Grade Calculator",
-    dirPath: "003-grade-calculator",
+    name: 'Grade Calculator',
+    dirPath: '003-grade-calculator',
   },
   {
-    name: "Number Table",
-    dirPath: "004-number-table",
+    name: 'Number Table',
+    dirPath: '004-number-table',
   },
   {
-    name: "Number Table of 1-10",
-    dirPath: "005-number-table-1-10",
+    name: 'Number Table of 1-10',
+    dirPath: '005-number-table-1-10',
   },
   {
-    name: "Employee Management",
-    dirPath: "006-employee-table",
+    name: 'Employee Management',
+    dirPath: '006-employee-table',
   },
   {
-    name: "Simple E-commerce",
-    dirPath: "007-e-commerce",
+    name: 'Simple E-commerce',
+    dirPath: '007-e-commerce',
   },
   {
-    name: "Render Products (Promise, async-await)",
-    dirPath: "008-render-products",
+    name: 'Render Products (Promise, async-await)',
+    dirPath: '008-render-products',
   },
 ];
 
 const contributors = [
   {
-    name: "BongoDev",
-    dirPath: "bongodev",
-    email: "support@bongodev.com",
+    name: 'BongoDev',
+    dirPath: 'bongodev',
+    email: 'support@bongodev.com',
   },
   {
-    name: "Nazma",
-    dirPath: "nazma98",
-    email: "nazma.sarker1398@gmail.com",
+    name: 'Nazma',
+    dirPath: 'nazma98',
+    email: 'nazma.sarker1398@gmail.com',
   },
   {
-    name: "Talha",
-    dirPath: "Talha",
-    email: "ameertalha22@gmail.com",
+    name: 'Talha',
+    dirPath: 'Talha',
+    email: 'ameertalha22@gmail.com',
   },
   {
-    name: "Md. Al Amin",
-    dirPath: "Alamin",
-    email: "aaalaamin7@gmail.com",
+    name: 'Md. Al Amin',
+    dirPath: 'Alamin',
+    email: 'aaalaamin7@gmail.com',
   },
   {
-    name: "Sumiya Yasmin",
-    dirPath: "sumiya-yasmin",
-    email: "sumiyayasmin665@gmail.com",
+    name: 'Sumiya Yasmin',
+    dirPath: 'sumiya-yasmin',
+    email: 'sumiyayasmin665@gmail.com',
   },
   {
-    name: "Than Win",
-    dirPath: "thanwin",
-    email: "thanwinhline84@gmail.com",
+    name: 'Than Win',
+    dirPath: 'thanwin',
+    email: 'thanwinhline84@gmail.com',
   },
   {
-    name: "Tamjid",
-    dirPath: "Tamjid",
-    email: "tamjidtj6@gmail.com",
+    name: 'Tamjid',
+    dirPath: 'Tamjid',
+    email: 'tamjidtj6@gmail.com',
   },
   {
-    name: "Dipu",
-    dirPath: "Dipu",
-    email: "mojumderdipu66@gmail.com",
+    name: 'Dipu',
+    dirPath: 'Dipu',
+    email: 'mojumderdipu66@gmail.com',
   },
   {
-    name: "Sakib",
-    dirPath: "Sakib",
-    email: "nazmussakib06927@gmail.com",
+    name: 'Sakib',
+    dirPath: 'Sakib',
+    email: 'nazmussakib06927@gmail.com',
   },
   {
-    name: "Kamrul",
-    dirPath: "Kamrul",
-    email: "nijumkamrul447@gmail.com",
+    name: 'Kamrul',
+    dirPath: 'Kamrul',
+    email: 'nijumkamrul447@gmail.com',
   },
   {
-    name: "Promi",
-    dirPath: "promi",
-    email: "farihapromi2611@gmail.com",
+    name: 'Promi',
+    dirPath: 'promi',
+    email: 'farihapromi2611@gmail.com',
   },
   {
-    name: "Shifat",
-    dirPath: "Shifat",
-    email: "shifatbinreza2@gmail.com",
+    name: 'Shifat',
+    dirPath: 'Shifat',
+    email: 'shifatbinreza2@gmail.com',
   },
   {
-    name: "MSI.Dipu",
-    dirPath: "MSI-DIPU",
-    email: "msidipu2@gmail.com",
+    name: 'MSI.Dipu',
+    dirPath: 'MSI-DIPU',
+    email: 'msidipu2@gmail.com',
   },
   {
-    name: "Mohyminul-Islam",
-    dirPath: "Mohyminul-Islam",
-    email: "mohyminulislam2001@gmail.com",
+    name: 'Mohyminul-Islam',
+    dirPath: 'Mohyminul-Islam',
+    email: 'mohyminulislam2001@gmail.com',
   },
   {
-    name: "Arman",
-    dirPath: "armaanepiic",
-    email: "armaanepiicfb17@gmail.com",
+    name: 'Arman',
+    dirPath: 'armaanepiic',
+    email: 'armaanepiicfb17@gmail.com',
   },
   {
-    name: "Asha",
-    dirPath: "Asha",
-    email: "afsanaasha502@gmail.com",
+    name: 'Asha',
+    dirPath: 'Asha',
+    email: 'afsanaasha502@gmail.com',
   },
   {
-    name: "Parisa Reza",
-    dirPath: "Parisa-Reza",
-    email: "rezaparisa5@gmail.com",
+    name: 'Parisa Reza',
+    dirPath: 'Parisa-Reza',
+    email: 'rezaparisa5@gmail.com',
   },
   {
-    name: "Tasin",
-    dirPath: "Tasin",
-    email: "ahmedtasin207@gmail.com",
+    name: 'Tasin',
+    dirPath: 'Tasin',
+    email: 'ahmedtasin207@gmail.com',
   },
   {
-    name: "foysal",
-    dirPath: "foysal1680",
-    email: "foysalkhanrial1680@gmail.com",
+    name: 'foysal',
+    dirPath: 'foysal1680',
+    email: 'foysalkhanrial1680@gmail.com',
   },
   {
-    name: "Mohammad Fahim Hossain",
-    dirPath: "md-fahim-hossain",
-    email: "mdfahimhossain2@gmail.com",
+    name: 'Mohammad Fahim Hossain',
+    dirPath: 'md-fahim-hossain',
+    email: 'mdfahimhossain2@gmail.com',
   },
   {
-    name: "Irfan",
-    dirPath: "irfan-mahbub",
-    email: "talha.mahbub.irfan@gmail.com",
+    name: 'Irfan',
+    dirPath: 'irfan-mahbub',
+    email: 'talha.mahbub.irfan@gmail.com',
   },
   {
-    name: "Jubayar Ahamad",
-    dirPath: "yet-yuvi",
-    email: "eng.jubayar@gmail.com",
+    name: 'Jubayar Ahamad',
+    dirPath: 'yet-yuvi',
+    email: 'eng.jubayar@gmail.com',
   },
   {
-    name: "Md. Ali Imam",
-    dirPath: "EmperorAlii",
-    email: "asvhon832@gmail.com",
+    name: 'Md. Ali Imam',
+    dirPath: 'EmperorAlii',
+    email: 'asvhon832@gmail.com',
   },
 ];
 
-const projectsContainer = document.getElementById("projects-container");
+const projectsContainer = document.getElementById('projects-container');
 
 async function checkDirectoryExists(projectPath, dirPath) {
   const filePath = `./${projectPath}/${dirPath}/index.html`;
 
   try {
-    const response = await fetch(filePath, { method: "HEAD" });
+    const response = await fetch(filePath, { method: 'HEAD' });
     return response.ok;
   } catch (error) {
     console.error(`Error checking file: ${filePath}`, error);
@@ -166,8 +166,8 @@ async function checkDirectoryExists(projectPath, dirPath) {
 }
 
 async function getContributors(projectPath) {
-  const contributorsList = document.createElement("ul");
-  contributorsList.className = "list-disc list-inside hidden ml-2";
+  const contributorsList = document.createElement('ul');
+  contributorsList.className = 'list-disc list-inside hidden ml-2';
 
   contributors.forEach(async (contributor) => {
     const directoryExists = await checkDirectoryExists(
@@ -175,13 +175,13 @@ async function getContributors(projectPath) {
       contributor.dirPath
     );
     if (directoryExists) {
-      const listItem = document.createElement("li");
+      const listItem = document.createElement('li');
 
-      const projectLink = document.createElement("a");
+      const projectLink = document.createElement('a');
       projectLink.className =
-        "text-blue-600 visited:text-purple-600 hover:underline";
+        'text-blue-600 visited:text-purple-600 hover:underline';
       projectLink.innerText = contributor.name;
-      projectLink.target = "_blank";
+      projectLink.target = '_blank';
       projectLink.href = `./${projectPath}/${contributor.dirPath}/index.html`;
 
       listItem.appendChild(projectLink);
@@ -193,33 +193,33 @@ async function getContributors(projectPath) {
 }
 
 function getProjectTitle(projectName) {
-  const projectTitle = document.createElement("h1");
-  projectTitle.className = "text-xl font-bold text-gray-800 mb-4";
+  const projectTitle = document.createElement('h1');
+  projectTitle.className = 'text-xl font-bold text-gray-800 mb-4';
   projectTitle.innerText = projectName;
   return projectTitle;
 }
 
 async function renderProjectsAndContributors() {
-  const projectsContainer = document.getElementById("projects-container");
+  const projectsContainer = document.getElementById('projects-container');
 
   projects.forEach(async (project) => {
-    const projectSection = document.createElement("div");
+    const projectSection = document.createElement('div');
     projectSection.classList.add(
-      "mx-auto",
-      "px-4",
-      "py-2",
-      "bg-gray-200",
-      "mb-2",
-      "project-sections",
-      "cursor-pointer",
-      "rounded-md"
+      'mx-auto',
+      'px-4',
+      'py-2',
+      'bg-gray-200',
+      'mb-2',
+      'project-sections',
+      'cursor-pointer',
+      'rounded-md'
     );
-    const projectHeader = document.createElement("div");
-    projectHeader.classList.add("flex", "justify-between");
+    const projectHeader = document.createElement('div');
+    projectHeader.classList.add('flex', 'justify-between');
     const projectTitle = getProjectTitle(project.name);
     projectHeader.appendChild(projectTitle);
-    const icon = document.createElement("i");
-    icon.classList.add("fa", "fa-angle-down", "my-auto", "text-gray-600");
+    const icon = document.createElement('i');
+    icon.classList.add('fa', 'fa-angle-down', 'my-auto', 'text-gray-600');
     projectHeader.append(icon);
 
     const contributors = await getContributors(project.dirPath);
@@ -230,15 +230,15 @@ async function renderProjectsAndContributors() {
   });
 }
 function handleDropdown() {
-  const projectSections = document.querySelectorAll(".project-sections");
+  const projectSections = document.querySelectorAll('.project-sections');
   projectSections.forEach((container) => {
-    container.addEventListener("click", (e) => {
-      container.children[1].classList.toggle("hidden");
+    container.addEventListener('click', (e) => {
+      container.children[1].classList.toggle('hidden');
       const icon = container.children[0].lastChild;
-      if (icon.classList.contains("fa-angle-down")) {
-        icon.classList.replace("fa-angle-down", "fa-angle-up");
+      if (icon.classList.contains('fa-angle-down')) {
+        icon.classList.replace('fa-angle-down', 'fa-angle-up');
       } else {
-        icon.classList.replace("fa-angle-up", "fa-angle-down");
+        icon.classList.replace('fa-angle-up', 'fa-angle-down');
       }
     });
   });
