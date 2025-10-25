525281d3108af9c9094bfbdda1a48a5124c62159
KaziNazmusSakib <kazinazmussakib36@gmail.com>
2025-10-25 23:54:37 +0600

    Change Counter-App

---
+++ 001-counter-app/Nazmus-Sakib007/script.js
@@ -0,0 +1,26 @@
+let count = 0;
+const counterElement = document.getElementById("counter");
+const incrementButton = document.getElementById("increment");
+const decrementButton = document.getElementById("decrement");
+
+function increaseCount() {
+  if(count === 10){
+    alert("Stop , Count is too much.");
+    return
+  }  
+  
+  count++;
+  counterElement.innerText = count;
+}
+
+function dcreaseCount() {
+  if (count <= 0){
+   alert("Stop, Count will be going to Negative.");
+  } else{
+  count--;
+  counterElement.innerText = count;
+  }
+}
+
+incrementButton.addEventListener("click", increaseCount);
+decrementButton.addEventListener("click", dcreaseCount);
