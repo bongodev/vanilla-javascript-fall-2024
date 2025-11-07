11c0287f6a74ec00955051a54c612a718f728093
foysal1680 <foysalkhanrial1680@gmil.com>
2025-11-07 16:38:59 +0600

    foysal1680:Add Employee Table

---
+++ 006-employee-table/foysal1680/script.js
@@ -0,0 +1,45 @@
+const employeeData = [
+  {
+    id: 1,
+    name: "John Doe",
+    position: "Software Engineer",
+    department: "Engineering",
+  },
+  {
+    id: 2,
+    name: "Jane Smith",
+    position: "Product Manager",
+    department: "Product",
+  },
+  { id: 3, name: "Sam Johnson", position: "Designer", department: "Design" },
+  {
+    id: 4,
+    name: "Chris Lee",
+    position: "Data Scientist",
+    department: "Data",
+  },
+];
+
+const employeeTableBody = document.getElementById("employeeTableBody");
+
+const getEmployeeRow = (employee) => {
+  const employeeRow = document.createElement("tr");
+
+  Object.keys(employee).forEach((key) => {
+    const column = document.createElement("td");
+    column.className = "border px-4 py-2";
+    column.innerText = employee[key];
+    employeeRow.appendChild(column);
+  });
+
+  return employeeRow;
+};
+
+const renderEmployeeData = (employees) => {
+  employees.forEach((employee) => {
+    const employeeRow = getEmployeeRow(employee);
+    employeeTableBody.appendChild(employeeRow);
+  });
+};
+
+renderEmployeeData(employeeData);
